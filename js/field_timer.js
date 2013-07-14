b17019d34b558328b69d4cf92bcf22670c242021
Sergei Brill <brilsergei@gmail.com>
2013-07-14 20:40:02 +0700

    county formatter added

--- js/field_timer.js
+++ js/field_timer.js
@@ -1,13 +1,29 @@
 (function($){
   Drupal.behaviors.field_timer = {
     attach: function() {
-      for (var type in Drupal.settings.field_timer) {
-        for (var id in Drupal.settings.field_timer[type]) {
-          for (var delta in Drupal.settings.field_timer[type][id]) {
-            $('#jquery-countdown-'+type+'-'+id+'-'+delta).not('.field-timer-processed').
-              countdown({
-                timestamp: new Date(Drupal.settings.field_timer[type][id][delta] * 1000),
-              }).addClass('.field-timer-processed');
+      var settings = Drupal.settings.field_timer;
+      for (var type in settings) {
+        for (var id in settings[type]) {
+          for (var delta in settings[type][id]) {
+            if (settings[type][id].plugin == 'jquery-countdown') {
+              $('#jquery-countdown-'+type+'-'+id+'-'+delta).not('.field-timer-processed').
+                countdown({
+                  timestamp: new Date(settings[type][id][delta] * 1000),
+                }).addClass('.field-timer-processed');
+            }
+              
+            if (settings[type][id].plugin == 'county') {
+              var options = settings[type][id].options;
+              $('#county-'+type+'-'+id+'-'+delta).not('.field-timer-processed').
+                county({
+                  endDateTime: new Date(settings[type][id][delta] * 1000),
+                  animation: options.animation,
+                  speed: options.speed,
+                  theme: options.theme,
+                  reflection: options.reflection,
+                  reflectionOpacity: options.reflectionOpacity,
+                }).addClass('.field-timer-processed');
+            }
           }
         }
       }
