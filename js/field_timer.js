a414e03b867b9bcdac24b2f51bdfa541134eb829
Sergei Brill <brilsergei@gmail.com>
2013-07-15 09:56:22 +0700

    jquery countdown formatter deleted

--- js/field_timer.js
+++ js/field_timer.js
@@ -5,13 +5,6 @@
       for (var type in settings) {
         for (var id in settings[type]) {
           for (var delta in settings[type][id]) {
-            if (settings[type][id].plugin == 'jquery-countdown') {
-              $('#jquery-countdown-'+type+'-'+id+'-'+delta).not('.field-timer-processed').
-                countdown({
-                  timestamp: new Date(settings[type][id][delta] * 1000),
-                }).addClass('.field-timer-processed');
-            }
-              
             if (settings[type][id].plugin == 'county') {
               var options = settings[type][id].options;
               $('#county-'+type+'-'+id+'-'+delta).not('.field-timer-processed').
