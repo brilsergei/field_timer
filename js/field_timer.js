15f57a88e43105bc34460ea6cc38ea321b61ebc2
Sergei Brill <brilsergei@gmail.com>
2013-08-13 19:27:36 +0700

    jQuery Countdown formatter added

--- js/field_timer.js
+++ js/field_timer.js
@@ -5,17 +5,36 @@
       for (var type in settings) {
         for (var id in settings[type]) {
           for (var delta in settings[type][id]) {
-            if (settings[type][id].plugin == 'county') {
-              var options = settings[type][id].options;
-              $('#county-'+type+'-'+id+'-'+delta).not('.field-timer-processed').
-                county({
-                  endDateTime: new Date(settings[type][id][delta] * 1000),
-                  animation: options.animation,
-                  speed: options.speed,
-                  theme: options.theme,
-                  reflection: options.reflection,
-                  reflectionOpacity: options.reflectionOpacity,
-                }).addClass('.field-timer-processed');
+            switch (settings[type][id].plugin) {
+              case 'county': {
+                var options = settings[type][id].options;
+                $('#county-'+type+'-'+id+'-'+delta).not('.field-timer-processed').
+                  county({
+                    endDateTime: new Date(settings[type][id][delta] * 1000),
+                    animation: options.animation,
+                    speed: options.speed,
+                    theme: options.theme,
+                    reflection: options.reflection,
+                    reflectionOpacity: options.reflectionOpacity,
+                  }).addClass('field-timer-processed');
+              }
+              
+              case 'jquery.countdown': {
+                var options = settings[type][id].options;
+                $('#jquery-countdown-'+type+'-'+id+'-'+delta).not('.field-timer-processed').
+                  countdown({
+                    until: options.until ? new Date(settings[type][id][delta] * 1000) : null,
+                    since: options.since ? new Date(settings[type][id][delta] * 1000) : null,
+                    format: options.format,
+                    layout: options.layout,
+                    compact: options.compact,
+                    significant: options.significant,
+                    timeSeparator: options.timeSeparator,
+                    description: options.description,
+                    expiryText: options.expiryText,
+                    expiryUrl: options.expiryUrl,
+                  }).addClass('field-timer-processed');
+              }
             }
           }
         }
