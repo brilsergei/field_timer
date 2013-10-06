45c6f6feeebba25a82f16e4fb1ccd551f39621f7
Sergei Bril <brilsergei@gmail.com>
2013-10-06 20:22:43 +0600

    add jQuery Countdown LED formatter

--- js/field_timer.js
+++ js/field_timer.js
@@ -34,6 +34,19 @@
                     expiryText: options.expiryText,
                     expiryUrl: options.expiryUrl,
                   }).addClass('field-timer-processed');
+                break;
+                
+              case 'jquery.countdown.led':
+                var options = settings[type][id].options;
+                $('#jquery-countdown-led-' + type + '-' + id + '-' + delta).not('.field-timer-processed').
+                  countdown({
+                    until: options.until ? new Date(settings[type][id][delta] * 1000) : null,
+                    since: options.since ? new Date(settings[type][id][delta] * 1000) : null,
+                    layout: $('#jquery-countdown-led-' + type + '-' + id + '-' + delta).html(),
+                    description: options.description,
+                    expiryText: options.expiryText,
+                    expiryUrl: options.expiryUrl,
+                  }).addClass('field-timer-processed');
             }
           }
         }
