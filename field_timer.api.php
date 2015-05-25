44bf1789263eae070c73da90aa6f900873f8755b
sergei_brill <brilsergei@gmail.com>
2015-05-25 22:20:15 +0500

    Some cleanup

--- field_timer.api.php
+++ field_timer.api.php
@@ -9,9 +9,9 @@
  * Allows modules to define date properties of an entity type.
  *
  * @return array
- * Keys of the array are entity types and values are arrays described
- * properties. Each key of subarray is property name and value is property
- * human readable name.
+ *   Keys of the array are entity types and values are arrays described
+ *   properties. Each key of subarray is property name and value is property
+ *   human readable name.
  *
  * @see field_timer_field_timer_entity_properties()
  */
--- js/field_timer.js
+++ js/field_timer.js
@@ -1,48 +1,53 @@
+/**
+ * @file
+ * File with JS to initialize jQuery plugins on fields.
+ */
+
 (function($){
   Drupal.behaviors.field_timer = {
     attach: function() {
       var settings = Drupal.settings.field_timer;
       for (var key in settings) {
-				switch (settings[key].plugin) {
-					case 'county':
-						var options = settings[key].options;
-						$('#county-' + key).not('.field-timer-processed').
-							county({
-								endDateTime: new Date(settings[key].timestamp * 1000),
-								animation: options.animation,
-								speed: options.speed,
-								theme: options.county_theme,
-								reflection: options.reflection,
-								reflectionOpacity: options.reflectionOpacity,
-							}).addClass('field-timer-processed');
-						break;
+        switch (settings[key].plugin) {
+          case 'county':
+            var options = settings[key].options;
+            $('#county-' + key).not('.field-timer-processed').
+              county({
+                endDateTime: new Date(settings[key].timestamp * 1000),
+                animation: options.animation,
+                speed: options.speed,
+                theme: options.county_theme,
+                reflection: options.reflection,
+                reflectionOpacity: options.reflectionOpacity,
+              }).addClass('field-timer-processed');
+            break;
 
-					case 'jquery.countdown':
-						var options = settings[key].options;
-						$('#jquery-countdown-' + key).not('.field-timer-processed').
-							countdown({
-								until: options.until ? new Date(settings[key].timestamp * 1000) : null,
-								since: options.since ? new Date(settings[key].timestamp * 1000) : null,
-								format: options.format,
-								layout: options.layout,
-								compact: options.compact,
-								significant: options.significant,
-								timeSeparator: options.timeSeparator,
-								padZeroes: options.padZeroes,
-							}).addClass('field-timer-processed');
-						break;
+          case 'jquery.countdown':
+            var options = settings[key].options;
+            $('#jquery-countdown-' + key).not('.field-timer-processed').
+              countdown({
+                until: options.until ? new Date(settings[key].timestamp * 1000) : null,
+                since: options.since ? new Date(settings[key].timestamp * 1000) : null,
+                format: options.format,
+                layout: options.layout,
+                compact: options.compact,
+                significant: options.significant,
+                timeSeparator: options.timeSeparator,
+                padZeroes: options.padZeroes,
+              }).addClass('field-timer-processed');
+            break;
 
-					case 'jquery.countdown.led':
-						var options = settings[key].options;
-						$('#jquery-countdown-led-' + key).not('.field-timer-processed').
-							countdown({
-								until: options.until ? new Date(settings[key].timestamp * 1000) : null,
-								since: options.since ? new Date(settings[key].timestamp * 1000) : null,
-								layout: $('#jquery-countdown-led-' + key).html(),
-							}).addClass('field-timer-processed');
-						break;
-				}
-			}
+          case 'jquery.countdown.led':
+            var options = settings[key].options;
+            $('#jquery-countdown-led-' + key).not('.field-timer-processed').
+              countdown({
+                until: options.until ? new Date(settings[key].timestamp * 1000) : null,
+                since: options.since ? new Date(settings[key].timestamp * 1000) : null,
+                layout: $('#jquery-countdown-led-' + key).html(),
+              }).addClass('field-timer-processed');
+            break;
+        }
+      }
     }
   }
 })(jQuery);
