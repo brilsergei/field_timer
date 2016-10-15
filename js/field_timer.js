dbbab603bed385235ccbf4931d9451a409fd3daf
pashupathinath <pashupathinath@2848619.no-reply.drupal.org>
2016-10-15 15:28:44 +0300

    Issue #2683927 by pashupathi nath gajawada: Field Timer JavaScript ESLint issues

--- js/field_timer.js
+++ js/field_timer.js
@@ -3,48 +3,51 @@
  * File with JS to initialize jQuery plugins on fields.
  */
 
-(function($){
+(function ($) {
+  "use strict";
   Drupal.behaviors.field_timer = {
-    attach: function() {
+    attach: function () {
       var settings = drupalSettings.field_timer;
-      if ($.countdown != undefined) {
+      if ($.countdown !== 'undefined') {
         $.countdown.setDefaults($.countdown.regionalOptions['']);
       }
       for (var key in settings) {
-        var options = settings[key].settings;
-        var $item = $('#' + key);
-        var timestamp = $item.data('timestamp');
-        switch (settings[key].plugin) {
-          case 'county':
-            $item.once('field-timer').each(function() {
-              $(this).county($.extend({endDateTime: new Date(timestamp * 1000)}, options));
-            });
-            break;
-
-          case 'jquery.countdown':
-            $item.once('field-timer').each(function() {
-              $(this).countdown($.extend(
-                options,
-                {
-                  until: (options.until ? new Date(timestamp * 1000) : null),
-                  since: (options.since ? new Date(timestamp * 1000) : null)
-                },
-                $.countdown.regionalOptions[options.regional]
-              ));
-            });
-            break;
-
-          case 'jquery.countdown.led':
-            $item.once('field-timer').each(function() {
-              $(this).countdown({
-                until: (options.until ? new Date(timestamp * 1000) : null),
-                since: (options.since ? new Date(timestamp * 1000) : null),
-                layout: $item.html()
+        if (settings.hasOwnProperty(key)) {
+          var options = settings[key].settings;
+          var $item = $('#' + key);
+          var timestamp = $item.data('timestamp');
+          switch (settings[key].plugin) {
+            case 'county':
+              $item.once('field-timer').each(function () {
+                $(this).county($.extend({endDateTime: new Date(timestamp * 1000)}, options));
               });
-            });
             break;
+
+            case 'jquery.countdown':
+              $item.once('field-timer').each(function () {
+                $(this).countdown($.extend(
+                  options,
+                  {
+                    until: (options.until ? new Date(timestamp * 1000) : null),
+                    since: (options.since ? new Date(timestamp * 1000) : null)
+                  },
+                  $.countdown.regionalOptions[options.regional]
+                ));
+              });
+            break;
+
+            case 'jquery.countdown.led':
+              $item.once('field-timer').each(function () {
+                $(this).countdown({
+                  until: (options.until ? new Date(timestamp * 1000) : null),
+                  since: (options.since ? new Date(timestamp * 1000) : null),
+                  layout: $item.html()
+                });
+              });
+            break;
+          }
         }
       }
     }
-  }
+  };
 })(jQuery);
